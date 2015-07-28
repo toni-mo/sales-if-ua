3f4f63bf3b8cd8995693c3bc62ea3af56eaebf72
KraytsMan <volodyakrajetsky@gmail.com>
2015-07-28 17:54:42 +0300

    paypal

---
+++ src/main/java/sales/paypal/repository/PaypalRepository.java
@@ -0,0 +1,15 @@
+package sales.paypal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sales.paypal.domain.Paypal;
+
+/**
+ * Created by volodya on 28.07.15.
+ */
+public interface PaypalRepository extends JpaRepository<Paypal, Integer> {
+
+    public Paypal findById(int id);
+
+    public void removeById(int id);
+
+}
