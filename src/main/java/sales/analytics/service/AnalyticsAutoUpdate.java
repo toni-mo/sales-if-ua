e2f7a5a167fac50674b0bf86f3d8f332a3b3ecdb
MyroslavDerevyanko <muroclav94@ukr.net>
2015-08-03 15:17:17 +0300

    added generator for analytics

--- src/main/java/sales/analytics/service/AnalyticsAutoUpdate.java
+++ src/main/java/sales/analytics/service/AnalyticsAutoUpdate.java
@@ -5,20 +5,30 @@ import org.springframework.scheduling.annotation.Scheduled;
 import sales.analytics.domain.Analytics;
 import sales.analytics.repository.AnalyticsRepository;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
+import java.util.logging.Logger;
 
 /**
  * Created by Myroslav on 30.07.2015.
  */
 public class AnalyticsAutoUpdate {
+    protected static Logger logger = Logger.getLogger(AnalyticsAutoUpdate.class.getName());;
     @Autowired
     private AnalyticsService analyticsService;
     private AnalyticsRepository analyticsRepository;
 
     @Scheduled(cron="50 23 * * * ?")
-    public void demoServiceMethod()
+    public void dailyUpdate()
     {
-        System.out.println("Fake date added");
-        analyticsService.save(new Analytics(5, 6000, 10, 15, new Date()));
+        Random rand = new Random();
+        logger.info("Daily update" + new Date());
+        analyticsRepository.save(new Analytics(100 + rand.nextInt(500), 20000 + rand.nextInt(980000), 100 + rand.nextInt(700), 15 + rand.nextInt(85), new Date()));
     }
+
+
+
 }
