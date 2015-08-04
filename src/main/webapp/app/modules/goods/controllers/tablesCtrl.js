2c0708b70ced50a3f283b737bac428837e84a63f
Andrii Zarin <zarin_andrii@mail.ru>
2015-08-04 16:23:08 +0300

    improved registration form & rewrited product page

--- src/main/webapp/app/modules/goods/controllers/tablesCtrl.js
+++ src/main/webapp/app/modules/goods/controllers/tablesCtrl.js
@@ -1,10 +1,10 @@
 goods.controller('tablesCtrl', ['$scope', '$http', function($scope, $http){
 
-	$http.get('modules/goods/json/nazar-shops-list.json').then(function(response){
+	$http.get('app/modules/goods/json/nazar-shops-list.json').then(function(response){
    	$scope.shops_list = response.data;
    	});
 
-   	$http.get('modules/goods/json/nazar-characteristic-list.json').then(function(response){
+   	$http.get('app/modules/goods/json/nazar-characteristic-list.json').then(function(response){
    	$scope.characteristic_list = response.data;
    	});
 
